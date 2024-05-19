927811c2d78ce781465b6b86a131e0c7e0a3de14
MatheusAlexandre20 <matheusalexdre1@outlook.com>
2024-05-19 13:01:08 -0300

    atualização de domingo, moedas funcionando

--- script.js
+++ script.js
@@ -3,16 +3,42 @@
 
 
 const convertButton = document.querySelector("#main-button")
-console.log(convertButton)
+const currencySelect = document.querySelector(".currency-select")
+
+function convertValues() {
+    const input = document.querySelector(".input-currency").value
+    const currencyValueToConvert = document.querySelector(".currency-value-to-convert") 
+    const currencyValueConverted = document.querySelector(".currency-value") 
+
+    console.log(currencySelect.value)
+
+    const dolar = 5.10
+    const euro = 5.56
 
 
-function convertValues(){
-    const input= document.querySelector(".input-currency").value
-    const dolar= 5.1
 
-const convertDolar= input / dolar
+    if(currencySelect.value == "Dolar") { 
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD"
+        }).format(input/dolar)
+    }
 
-console.log(convertDolar)
+    if(currencySelect.value == "Euro") {
+
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
+            style:"currency",
+            currency: "EUR"
+        }).format(input/euro)
+    }
+
+
+    currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    }).format(input)
+
+
 }
 
 
